9e0efc356e1ee1c6c4c08ec16ef33231d6f6b5e1
Иван Тимофеев <tutktoto05@gmail.com>
2025-03-15 10:22:20 +0300

    new img

--- src/App.vue
+++ src/App.vue
@@ -126,14 +126,14 @@ export default {
         </div>
         <div class="main-fourth__img">
           <div class="main-fourth__img-conteiner">
-            <img src="./assets/imgs/anime_girl2.jpg" alt="" />
+            <img src="./assets/imgs/anime_girl.jpg" alt="" />
           </div>
         </div>
       </div>
       <div class="main-fourth" v-if="!isMobile">
         <div class="main-fourth__img">
           <div class="main-fourth__img-conteiner">
-            <img src="./assets/imgs/anime_girl2.jpg" alt="" />
+            <img src="./assets/imgs/anime_girl.jpg" alt="" />
           </div>
         </div>
         <div class="main-fourth__text_block">
